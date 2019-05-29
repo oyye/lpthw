b09d13d5a5d397b0ead813d5682c0dd0ecea0d17
oyye <33031902+oyye@users.noreply.github.com>
2019-05-29 09:43:14 -0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,12 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+
+total = my_age + my_height + round(my_weight * 0.45359237)
+print(f"If I add {my_age}, {my_height}, and {round(my_weight * 0.45359237)} kg I get {total}")
